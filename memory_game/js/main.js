89b4246ed443882895bffc4ea6a1568fde5ffbc8
samahab <samahab.TechCampus@gmail.com>
2019-04-14 08:47:17 +0300

    create game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,23 +1,28 @@
 
-//var cardOne = cards[0];
-//var cardTwo = "queen";
-//var cardThree = "king";
-//var cardFour ="king";
 
 
 
 var cards=["king","king","queen","queen"];
-var cardsInPlay=[cardOne,cardTwo];
-var cardOne =cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-var cardTwo =cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king")
-
-if(cardsInPlay[0]===cardsInPlay[1]){
+var cardsInPlay=[];
+var checkForMatch= function(){
+	if(cardsInPlay[0]===cardsInPlay[1]){
 	alert("You found a match");
 }
 else {
 	alert("sorry try agin");
 }
+
+}
+
+var flipCard =function(cardId){
+	
+	console.log("User flipped "+cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+
+	checkForMatch();
+
+};
+
+flipCard(0);
+flipCard(2);
+
